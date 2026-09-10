f242ea57dd9fc12b77a3a860d33fb0779a82451c
Youssef <ibrahimyoussef42@gmail.com>
2026-09-10 10:19:44 +0100

    new file

---
+++ presentation3.js
@@ -0,0 +1,9 @@
+const prompt =require("prompt-sync")();
+
+let nom = prompt("Quel est votre nom ?");
+console.log("bonjour", nom);
+
+let prenom = prompt("Quel est votre prenom ?");
+console.log("bonjour", prenom);
+let formation = prompt("Quel est votre formation ?");
+console.log("boncourage", formation);
